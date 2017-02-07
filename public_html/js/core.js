3d56efbad5ca5327d11576e6bfd8288781f973c9
gesucca <simcip.ssdev@gmail.com>
2017-02-07 09:08:24 +0100

    major js restructuring

---
+++ public_html/js/core.js
@@ -0,0 +1,360 @@
+/*OBJECTS DEFINITIONS*/
+
+var FirstMap = function() {
+
+	var innerMap = {
+		'A': 1,
+		'B': 2,
+		'C': 3,
+		'D': 4,
+		'E': 5,
+		'F': 6,
+		'G': 7,
+		'H': 8,
+		'I': 9,
+		'J': 10,
+		'K': 11,
+		'L': 12,
+		'M': 13,
+		'N': 14,
+		'O': 15,
+		'P': 16,
+		'Q': 17,
+		'R': 18,
+		'S': 19,
+		'T': 20,
+		'U': 21,
+		'V': 22,
+		'W': 23,
+		'X': 24,
+		'Y': 25,
+		'Z': 26,
+		'1': 27,
+		'2': 28,
+		'3': 29,
+		'4': 30,
+		'5': 31,
+		'6': 32,
+		'7': 33,
+		'8': 34,
+		'9': 35,
+		'0': 36,
+		'?': 37,
+		'!': 38,
+		'\'': 39,
+		'\n': 40,
+		' ': 41,
+		'.': 42,
+		':': 43,
+		',': 44,
+		';': 45, // expiration char
+		'-': 46, // length char
+		'_': 47 // this is a void one, without it conversion will not work!
+	};
+
+	this.get = function() {
+		return innerMap;
+	}
+
+	this.getReverse = function() {
+		return reverse(innerMap);
+	};
+}
+
+var LastMap = function() {
+	var innerMap = {
+		0: ' ',
+		1: 'a',
+		2: 'b',
+		3: 'c',
+		4: 'd',
+		5: 'e',
+		6: 'f',
+		7: 'g',
+		8: 'h',
+		9: 'i',
+		10: 'j',
+		11: 'k',
+		12: 'l',
+		13: 'm',
+		14: 'n',
+		15: 'o',
+		16: 'p',
+		17: 'q',
+		18: 'r',
+		19: 's',
+		20: 't',
+		21: 'u',
+		22: 'v',
+		23: 'w',
+		24: 'x',
+		25: 'y',
+		26: 'z',
+		27: 'A',
+		28: 'B',
+		29: 'C',
+		30: 'D',
+		31: 'E',
+		32: 'F',
+		33: 'G',
+		34: 'H',
+		35: 'I',
+		36: 'J',
+		37: 'K',
+		38: 'L',
+		39: 'M',
+		40: 'O',
+		41: 'N',
+		42: 'P',
+		43: 'Q',
+		44: 'R',
+		45: 'S',
+		46: 'T',
+		47: 'U',
+		48: 'V',
+		49: 'W',
+		50: 'X',
+		51: 'Y',
+		52: 'Z',
+		53: '1',
+		54: '2',
+		55: '3',
+		56: '4',
+		57: '5',
+		58: '6',
+		59: '7',
+		60: '8',
+		61: '9',
+		62: '0'
+	}
+
+	this.get = function() {
+		return innerMap;
+	}
+
+	this.getReverse = function() {
+		return reverse(innerMap);
+	};
+}
+
+class ToTen {
+
+	constructor(basicMap) {
+		this._basicMap = basicMap;
+		this._basic = Object.keys(basicMap).length;
+	}
+
+	convert(msg) {
+		var output = 0,
+			pos = null;
+		for (var i = 0; i < msg.length; i++) {
+			pos = msg[i];
+			output += (Math.pow(this._basic, i) * this._basicMap[pos]);
+		}
+		return output;
+	}
+}
+
+class ToMap {
+
+	constructor(targetMap) {
+		this._tMap = targetMap;
+		this._target = Object.keys(targetMap).length;
+	}
+
+	convert(int) {
+		var converted = [],
+			rem = 0;
+		while (Math.floor(int / this._target) > 0) {
+			rem = int % this._target;
+			converted.push(this._tMap[rem]);
+			int = Math.floor(int / this._target);
+		}
+		converted.push(this._tMap[int]);
+		return converted;
+	}
+}
+
+var Slicer = function(msg) {
+
+	var array = msg.split('');
+
+	var i = 0;
+	var finish = false;
+
+	this.getSlice = function() {
+
+		var output = [];
+
+		if (i == array.length)
+			finish = true;
+
+		if (finish)
+			return output;
+
+		// ok, I won't refactor this, let's document it:
+		// if 5 elements or more left, take everything
+		if (i + 5 <= array.length) {
+			output = [array[i + 0], array[i + 1], array[i + 2], array[i + 3], array[i + 4]];
+			i += 5;
+		}
+		// if less than 5 elements has yet to be considered...
+		else if ((array.length - i) < 5) {
+			// take everything left
+			for (; i < array.length; i++) {
+				output.push(array[i]);
+			}
+			//then fill up to five
+			for (k = output.length; k < 5; k++) {
+				output.push(' ');
+			}
+		}
+
+		return output;
+	}
+
+	this.isEnd = function() {
+		return finish;
+	}
+}
+
+
+var Builder = function() {
+
+	var built = '';
+
+	this.get = function() {
+		return built;
+	}
+
+	this.append = function(slice) {
+
+		if (slice.length > 5)
+		// nice, the only error thrown is the only one that keeps spawning in unit test from time to time
+			throw "ERROR BUILDER: Slice too long! " + slice;
+
+		if (slice.length < 5) {
+			for (i = slice.length; i < 5; i++) {
+				slice.push(' ');
+			}
+		}
+
+		built += slice.join('');
+	}
+}
+
+class Expirator {
+
+	constructor(time, msg) {
+		this._time = time;
+		this._msg = msg;
+		this._now = new Date().getTime();
+	}
+
+	getExpMsg() {
+		return (';' + this._time + ';' + this._now + ';' + this._msg);
+	}
+}
+
+var ExpChecker = function(msg) {
+
+	var dirtyMsg = msg;
+
+	function needsExpCheck() {
+		if (dirtyMsg.indexOf(';') == 0)
+			return true;
+		else
+			return false;
+	}
+
+	function isExpired() {
+
+		var checkMsg = dirtyMsg;
+
+		function getLimit() {
+			checkMsg = checkMsg.substring(checkMsg.indexOf(';') + 1, checkMsg.length);
+			return checkMsg.substring(0, checkMsg.indexOf(';')) * 3600000;
+		}
+
+		function getOriginTime() {
+			checkMsg = checkMsg.substring(checkMsg.indexOf(';') + 1, checkMsg.length);
+			return parseInt(checkMsg.substring(0, checkMsg.indexOf(';')));
+		}
+
+		var hourLimitMs = getLimit();
+		var origTime = getOriginTime();
+
+		var now = new Date().getTime();
+
+		if (now - origTime > hourLimitMs)
+			return true;
+		else
+			return false;
+	}
+
+	function clean(dirtyMsg) {
+		// cut away everyting before the third ;
+		for (i = 0; i < 3; i++)
+			dirtyMsg = dirtyMsg.substring(dirtyMsg.indexOf(';') + 1, dirtyMsg.length);
+
+		return dirtyMsg;
+	}
+
+	this.getMsgExpChecked = function() {
+
+		var expAlert = 'THE MESSAGE BLEW UP!\n \nIT CANNOT BE DECIPHERED ANYMORE!';
+
+		if (!needsExpCheck())
+			return dirtyMsg;
+
+		if (isExpired()) {
+			alert(expAlert);
+			return '54-' + expAlert;
+		}
+
+		return clean(dirtyMsg);
+	}
+}
+
+
+/*FUNCTIONS*/
+
+// String hashing: thanks SO
+String.prototype.hashCode = function() {
+	var hash = 0,
+		i, chr, len;
+	if (this.length === 0) return hash;
+	for (i = 0, len = this.length; i < len; i++) {
+		chr = this.charCodeAt(i);
+		hash = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+};
+
+function getMagicNumber(usn, pwd) {
+	var theMagicNumber = usn.hashCode() + pwd.hashCode();
+	return Math.abs(theMagicNumber) % 9991;
+}
+
+//regex magic to get rid of double spaces
+function noDoubleSpace(msg) {
+	return msg.replace(/\s\s+/g, ' ');
+}
+
+// it gets rid of those phantom chars at the end
+function decodeLength(msg) {
+	var splitPoint = msg.indexOf('-');
+	var l = parseInt(msg.substring(0, splitPoint));
+
+	return msg.substring(splitPoint + 1, splitPoint + 1 + l);
+}
+
+//reversees an object: used for maps
+function reverse(map) {
+	var ret = {};
+	for (var key in map) {
+		ret[map[key]] = key;
+	}
+	return ret;
+};
---
+++ public_html/js/front.js
@@ -0,0 +1,226 @@
+/*OBJECTS DEFINITIOPNS*/
+
+class Checker {
+
+    constructor(msg, chars) {
+        this._msg = msg;
+        this._chars = chars;
+    }
+
+    //popups text will be defined in subclasses
+    check() {
+        if (!this._msg) {
+            if (this.lang == 'en')
+                alert(this.popUpVoidEN);
+            if (this.lang == 'it')
+                alert(this.popUpVoidIT);
+            return false;
+        }
+        if (!this._chars.test(this._msg)) {
+            if (this.lang == 'en')
+                alert(this.popUpIllegalEN);
+            if (this.lang == 'it')
+                alert(this.popUpIllegalIT);
+            return false;
+        }
+        return true;
+    }
+
+}
+
+class MsgChecker extends Checker {
+
+    constructor(msg, chars) {
+        super(msg, chars);
+
+        this.popUpVoidEN = 'Your message is empty!\n \nNothing will be done...\n ';
+        this.popUpIllegalEN = 'Your message contains illegal characters.\n \nOnly plain letters, numbers and basic puntuation are permitted.\n ';
+
+        this.popUpVoidIT = 'Il tuo messaggio è vuoto!\n \nNon cripto proprio niente...\n ';
+        this.popUpIllegalIT = 'Il tuo messaggio contiene caratteri non permessi.\n \nUsa solo lettere senza accenti, numeri e punteggiatura di base.\n ';
+    }
+}
+
+class FieldChecker extends Checker {
+
+    constructor(fieldName, fieldValue) {
+        super(fieldValue, /^[a-zA-Z0-9'?!,.:;\n ]*$/);
+
+        this._fieldName = fieldName;
+        this._fieldValue = fieldValue;
+        this._minLength = 7;
+
+        this.popUpVoidEN = 'The field \'' + fieldName + '\' is empty!\n ';
+        this.popUpIllegalEN = 'Your ' + fieldName + ' contains illegal characters.\n \nOnly plain letters, numbers and basic puntuation are permitted.\n ';
+        this.popUpShortEN = 'Your ' + fieldName + ' is too short!\n \nChoose one with at least ' + this._minLength + ' characters.\n ';
+
+        this.popUpVoidIT = 'Il campo \'' + fieldName + '\' è vuoto!\n ';
+        this.popUpIllegalIT = 'Il campo\'' + fieldName + '\' messaggio contiene caratteri non permessi.\n \nUsa solo lettere senza accenti, numeri e punteggiatura di base.\n ';
+        this.popUpShortIT = 'Il campo \'' + fieldName + '\' è composto da troppi pochi caratteri!\n \nDevono essere almeno ' + this._minLength + ' per poter cifrare un messaggio.\n ';
+    }
+
+    check() {
+        var preResult = super.check();
+        if (!preResult)
+            return preResult;
+
+        if (this._fieldValue.length < this._minLength) {
+            if (this.lang == 'en')
+                alert(this.popUpShortEN);
+            if (this.lang == 'it')
+                alert(this.popUpShortIT);
+            return false;
+        }
+
+        return true;
+    }
+}
+
+
+/*FUNCTIONS*/
+
+function setLanguage(lang) {
+    var known = { en: true, it: true };
+    if (!known[lang])
+        lang = 'en';
+
+    //switch all divs class ltext 
+    $('div.ltext[lang=' + lang + ']').show();
+    $('div.ltext[lang!=' + lang + ']').hide();
+
+    //switch unreachable text, such as placeholders
+    if (lang == 'en') {
+        $('form[class=expiration_form]').attr('title', 'N.B.:  0 hours means the message will never self-destruct');
+        $('button[class=send]').attr('title', 'This may not work on certain browsers or devices!');
+        $('textarea[id=clearText]').attr('placeholder', 'The clear message goes here...');
+        $('textarea[id=cryptText]').attr('placeholder', 'The crypted message goes here...');
+        $('input[id=usn]').attr('placeholder', 'User Code');
+
+        //for the popups
+        Checker.prototype.lang = 'en';
+    }
+    if (lang == 'it') {
+        $('form[class=expiration_form]').attr('title', 'N.B.:  0 ore significa che il messaggio non si autodistruggerà mai');
+        $('button[class=send]').attr('title', 'Questa cosa potrebbe non funzionare su qualche browser o dispositivo!');
+        $('textarea[id=clearText]').attr('placeholder', 'Scrivi qui il messaggio in chiaro...');
+        $('textarea[id=cryptText]').attr('placeholder', 'Incolla qui il messaggio cifrato...');
+        $('input[id=usn]').attr('placeholder', 'Codice Utente');
+
+        //for the popups
+        Checker.prototype.lang = 'it';
+    }
+};
+
+// thanks SO
+function copyToClipboard(elem) {
+    // create hidden text element, if it doesn't already exist
+    var targetId = "_hiddenCopyText_";
+    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+    var origSelectionStart, origSelectionEnd;
+    if (isInput) {
+        // can just use the original source element for the selection and copy
+        target = elem;
+        origSelectionStart = elem.selectionStart;
+        origSelectionEnd = elem.selectionEnd;
+    } else {
+        // must use a temporary form element for the selection and copy
+        target = document.getElementById(targetId);
+        if (!target) {
+            var target = document.createElement("textarea");
+            target.style.position = "absolute";
+            target.style.left = "-9999px";
+            target.style.top = "0";
+            target.id = targetId;
+            document.body.appendChild(target);
+        }
+        target.textContent = elem.textContent;
+    }
+    // select the content
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+
+    // copy the selection
+    var succeed;
+    try {
+        succeed = document.execCommand("copy");
+    } catch (e) {
+        succeed = false;
+    }
+    // restore original focus
+    if (currentFocus && typeof currentFocus.focus === "function") {
+        currentFocus.focus();
+    }
+
+    if (isInput) {
+        // restore prior selection
+        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+    } else {
+        // clear temporary content
+        target.textContent = "";
+    }
+    return succeed;
+}
+
+function sendEmail() {
+    window.location.href = "mailto:?body=" + getCryptMsg();
+}
+
+function sendWhatsApp() {
+    window.location.href = "whatsapp://send?text=" + getCryptMsg();
+}
+
+function cryptButton() {
+    var m = new MsgChecker(getClearMsg(), /^[A-Z0-9'?!,.:\n ]*$/, true);
+    if (!checkFor(m))
+        return;
+
+    var result = crypt(getClearMsg(), getUsn(), getPwd(), getExpiration());
+    writeMsg(result, 'cryptText');
+}
+
+function decryptButton() {
+    var m = new MsgChecker(getCryptMsg(), /^[a-zA-Z0-9 ]*$/, true);
+    if (!checkFor(m)) {
+        return;
+    }
+
+    var result = decrypt(getCryptMsg(), getUsn(), getPwd(), getExpiration());
+    writeMsg(result, 'clearText');
+}
+
+function getCryptMsg() {
+    return document.getElementById('cryptText').value;
+}
+
+function getClearMsg() {
+    return document.getElementById('clearText').value.toUpperCase();
+}
+
+function writeMsg(msg, id) {
+    document.getElementById(id).value = msg;
+}
+
+function getExpiration() {
+    var a = document.getElementById('expiration').value;
+    if (!a)
+        return 0;
+    else return a;
+}
+
+function getUsn() {
+    return document.getElementById('usn').value;
+}
+
+function getPwd() {
+    return document.getElementById('pwd').value;
+}
+
+function checkFor(msgCheck) {
+    var u = new FieldChecker('User Code', getUsn());
+    var p = new FieldChecker('password', getPwd());
+
+    if (!msgCheck.check() || !u.check() || !p.check())
+        return false;
+    return true;
+}
